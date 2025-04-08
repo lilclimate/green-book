0823623312d456e97b0bfe31cfb581e2d36c86de
zyx <zyx@yopoint.com>
2025-04-08 23:11:08 +0800

    feat: add auth routes and post detail page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -71,7 +71,7 @@ export default function TabLayout() {
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person" color={color} />,
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="person.fill" color={color} />,
         }}
       />
      
